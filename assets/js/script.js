1f1e42d899ce8ffabddc7d5aa8a1aea2ee7fd4de
Corfucinas <53630064+Corfucinas@users.noreply.github.com>
2021-06-06 17:22:47 +0800

    remove venobox

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,36 +1,36 @@
-  // Preloader js    
-  $(window).on('load', function () {
-    $('.preloader').fadeOut(100);
+// Preloader js
+$(window).on('load', function () {
+  $('.preloader').fadeOut(100);
+});
+
+(function ($) {
+  'use strict';
+
+  //  Search Form Open
+  $('#searchOpen').on('click', function () {
+    $('.search-wrapper').addClass('open');
+  });
+  $('#searchClose').on('click', function () {
+    $('.search-wrapper').removeClass('open');
   });
 
-  (function ($) {
-    'use strict';
+  // featured post slider
+  $('.featured-post-slider').slick({
+    infinite: true,
+    vertical: true,
+    verticalSwiping: true,
+    arrows: false,
+    dots: true,
+    responsive: [{
+      breakpoint: 600,
+      settings: {
+        vertical: false,
+        verticalSwiping: false,
+      }
+    }]
+  });
 
-    //  Search Form Open
-    $('#searchOpen').on('click', function () {
-      $('.search-wrapper').addClass('open');
-    });
-    $('#searchClose').on('click', function () {
-      $('.search-wrapper').removeClass('open');
-    });
+  // // venobox initialize
+  // $('.venobox').venobox();
 
-    // featured post slider
-    $('.featured-post-slider').slick({
-      infinite: true,
-      vertical: true,
-      verticalSwiping: true,
-      arrows: false,
-      dots: true,
-      responsive: [{
-        breakpoint: 600,
-        settings: {
-          vertical: false,
-          verticalSwiping: false,
-        }
-      }]
-    });
-
-    // venobox initialize
-    $('.venobox').venobox();
-
-  })(jQuery);
+})(jQuery);
